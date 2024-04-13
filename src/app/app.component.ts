a0a9a6ba562f9328f2fd342b51ae00aa8de583a1
hector <hector.ldz@gmail.com>
2024-04-13 14:09:13 +0200

    estructura inicial de modulos y componentes creada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SharedModule } from './shared/shared.module';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { LogoComponent } from './shared/logo/logo.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { LandingComponent } from './main/landing/landing.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SharedModule, NavbarComponent, LogoComponent, FooterComponent, LandingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
