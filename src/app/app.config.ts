2cffc41edcf2b08c77ea37b9385cd4819b3c0832
Ibizus <hector.ldz@gmail.com>
2024-05-26 00:21:06 +0100

    Try to fix subrouting and create Task service and ToDo component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideClientHydration } from '@angular/platform-browser';
 import {
   HttpClient,
   HttpClientModule,
@@ -12,8 +10,6 @@ import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), 
-    provideAnimationsAsync(),
-    provideClientHydration(),
     provideHttpClient(withFetch()), 
     HttpClientModule,
     HttpClient]
--- src/app/pages/todos/todos.component.ts
+++ src/app/pages/todos/todos.component.ts
@@ -1,13 +1,71 @@
 import { Component } from '@angular/core';
-import { DashboardComponent } from "../dashboard/dashboard.component";
+import { Task } from '../../utils/types';
+import { TaskService } from '../../services/task.service';
+import { TableModule } from 'primeng/table';
+import { PaginatorModule } from 'primeng/paginator';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
 
 @Component({
     selector: 'app-todos',
     standalone: true,
     templateUrl: './todos.component.html',
     styleUrl: './todos.component.scss',
-    imports: [DashboardComponent]
+    imports: [
+        TableModule,
+        PaginatorModule,
+        IconFieldModule,
+        InputIconModule,
+    ]
 })
 export class TodosComponent {
 
+    tasksList !: Task[];
+    // Pagination variables with default values:
+    first: number = 0;
+    rows: number = 10;
+    currentPage: number = 20;
+    demandedPage: number = 0;
+    totalPages: number = 20;
+    totalRecords: number = 200;
+    filter: string = '';
+
+    constructor(
+        private taskService: TaskService
+    ){}
+
+    ngOnInit() {
+      this.getTasks(this.demandedPage, this.rows, '')
+    }
+
+    paginate(event: any) {
+      this.first = event.page * event.rows;
+      this.rows = event.rows;
+      this.demandedPage = event.page; //Index of the new page
+      this.getTasks(this.demandedPage, this.rows, this.filter);
+    }
+
+    // Search bar event:
+    onInputChange(event: Event): void {
+      const filterValue = (event.target as HTMLInputElement).value;
+      this.filter = filterValue;
+      this.getTasks(0, this.rows, this.filter);
+    }
+
+    getTasks(page: number, size: number, filter: string) {
+        this.taskService.getTasks(page, size, filter).subscribe({
+          next: (data) => {
+            this.tasksList = data.tasks;
+            // CHECK DATA:
+            console.log(data);
+            console.log(this.tasksList);
+            // Pagination:
+            this.first = page * size + 1; // Calculate index of first item shown
+            this.rows = size; // Update size input from the request
+            this.currentPage = data.currentPage + 1; // In back first page is index 0
+            this.totalRecords = data.totalItems;
+          },
+          error: (error) => {console.error(error)},
+        });
+      }
 }
